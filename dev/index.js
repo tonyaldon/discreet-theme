2051cfa23357686eebcf830fdd7962c3b68a6752
tony <tony.aldon.adm@gmail.com>
2021-12-31 13:50:52 +0100

    remove file `dev/index.js`
    no longer needed for development

--- dev/index.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.use(express.static(path.join(__dirname, 'public')))
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
